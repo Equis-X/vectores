be8d616a3b32d85448399d37b904a1be06371ea4
Equis-X <hguzmanpujols@gmail.com>
2021-12-06 21:57:32 -0400

    Add project files.

---
+++ vectores/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace vectores
+{
+    public class Vector
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public Vector(int f, int s)
+        {
+            x = f;
+            y = s;
+        }
+        public double Magnitude()
+        {
+            return Math.Sqrt(Math.Pow(x, 2)+ Math.Pow(y, 2));
+        }
+    }
+}
